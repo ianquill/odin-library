c45453bfdfad8d5771e04475dbfac8d12cfd330a
ianquill <ian@ianquill.com>
2021-10-05 15:47:02 -0700

    Changed to classes

--- script.js
+++ script.js
@@ -31,12 +31,21 @@ function addBookToLibrary(title, author, pages, read) {
     populateStorage();
 }
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
+
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
 
 function refreshLibrary() {
     myLibrary.forEach((Book) => {
